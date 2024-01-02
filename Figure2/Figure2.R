c458b9cfce23bb4afeadf7cda869e1c08dc4247c
henni164 <51927997+henni164@users.noreply.github.com>
2024-01-02 17:12:23 +1100

    updating figure code

--- Figure2/Figure2.R
+++ Figure2/Figure2.R
@@ -30,74 +30,81 @@ ggsave("Figure2a.tiff", chr_lengths_plot, device = "tiff", height = 4.5, width =
 
 ## Figure 2b chr9 region dotplots
 
-hap8vshap21 <- read.delim("hap25_vs_hap13_chr9.paf", sep = "\t", header = FALSE)
-hap8vshap21 <- hap8vshap21[,-(13:17)]
-colnames(hap8vshap21) <- c("qname", "qlength", "qstart", "qend", "strand", "tname", "tlength", "tstart", "tend", "nmatches", "alnlength", "mquality")
-hap8vshap21 <- hap8vshap21[grep("chr", hap8vshap21$qname),]
+hap25vshap13chr9 <- read.delim("C:/Users/HEN294/OneDrive - CSIRO/Documents/Research/Current_projects/Pca_population_study/Mating_loci/hap25_vs_hap13_chr9.paf", sep = "\t", header = FALSE)
+hap25vshap13chr9 <- hap25vshap13chr9[,-(13:17)]
+colnames(hap25vshap13chr9) <- c("qname", "qlength", "qstart", "qend", "strand", "tname", "tlength", "tstart", "tend", "nmatches", "alnlength", "mquality")
+hap25vshap13chr9 <- hap25vshap13chr9[grep("chr", hap25vshap13chr9$qname),]
 
-hap6vshap23 <- read.delim("hap26_vs_hap27_chr9.paf", sep = "\t", header = FALSE)
-hap6vshap23 <- hap6vshap23[,-(13:17)]
-colnames(hap6vshap23) <- c("qname", "qlength", "qstart", "qend", "strand", "tname", "tlength", "tstart", "tend", "nmatches", "alnlength", "mquality")
+hap26vshap27chr9 <- read.delim("C:/Users/HEN294/OneDrive - CSIRO/Documents/Research/Current_projects/Pca_population_study/Mating_loci/hap26_vs_hap27_chr9.paf", sep = "\t", header = FALSE)
+hap26vshap27chr9 <- hap26vshap27chr9[,-(13:17)]
+colnames(hap26vshap27chr9) <- c("qname", "qlength", "qstart", "qend", "strand", "tname", "tlength", "tstart", "tend", "nmatches", "alnlength", "mquality")
 
 
-hap5vshap24 <- read.delim("hap26_vs_hap13_chr9.paf", sep = "\t", header = FALSE)
-hap5vshap24 <- hap5vshap24[,-(13:17)]
-colnames(hap5vshap24) <- c("qname", "qlength", "qstart", "qend", "strand", "tname", "tlength", "tstart", "tend", "nmatches", "alnlength", "mquality")
-hap5vshap24 <- hap5vshap24[grep("chr", hap5vshap24$qname),]
+hap26vshap13chr9 <- read.delim("C:/Users/HEN294/OneDrive - CSIRO/Documents/Research/Current_projects/Pca_population_study/Mating_loci/hap26_vs_hap13_chr9.paf", sep = "\t", header = FALSE)
+hap26vshap13chr9 <- hap26vshap13chr9[,-(13:17)]
+colnames(hap26vshap13chr9) <- c("qname", "qlength", "qstart", "qend", "strand", "tname", "tlength", "tstart", "tend", "nmatches", "alnlength", "mquality")
+hap26vshap13chr9 <- hap26vshap13chr9[grep("chr", hap26vshap13chr9$qname),]
 
-hap5vshap24_plot <- ggplot(data = hap5vshap24) +
-  geom_segment(data = hap5vshap24, aes(x = qstart, xend = qend, y = tstart, yend = tend), linewidth = 0.1) +
-  scale_y_continuous(position = "left", expand = c(0,0), labels = function(x)x/1000000) +
-  scale_x_continuous(position = "bottom", expand = c(0,0), labels = function(x)x/1000000) +
-  scale_color_gradient(low = "#D5D5D5", high = "#000000", aesthetics = "color", name = "%identity", labels = c("0","25","50","75","100")) +
-  coord_cartesian(xlim = c(1600000,3000000), ylim = c(1500000,2700000)) +
-  labs(x = "hap13", y = "hap26") +
-  theme(axis.text = element_text(color = "black", size = 6),
-        axis.title.x = element_text(color = "black", size = 8),
-        axis.title.y = element_text(color = "black", size = 8),
-        panel.background = element_blank(),
-        legend.position = "none",
-        panel.border = element_rect(fill = NA, color = "black", linetype = "dashed", linewidth = 0.1),
-        plot.margin = unit(c(0.5,0.3,0.3,0.3), units = "lines"))
-
-hap8vshap21_plot <- ggplot(data = hap8vshap21) +
-  geom_segment(data = hap8vshap21, aes(x = qstart, xend = qend, y = tstart, yend = tend), linewidth = 0.1) +
-  scale_y_continuous(position = "left", expand = c(0,0), labels = function(x)x/1000000) +
-  scale_x_continuous(position = "bottom", expand = c(0,0), labels = function(x)x/1000000) +
-  scale_color_gradient(low = "#D5D5D5", high = "#000000", aesthetics = "color", name = "% identity", limits = c(0,100), breaks = c(0,25,50,75,100), labels = c("0","25","50","75","100")) +
+hap25vshap13chr9_plot <- ggplot(data = hap25vshap13chr9) +
+  geom_segment(data = hap25vshap13chr9, aes(x = qstart, xend = qend, y = tstart, yend = tend), color = "black", linewidth = 0.3) +
+  scale_y_continuous(position = "left", expand = c(0,0), labels = function(x)x/1000000, breaks = c(2000000,2300000,2600000), sec.axis = dup_axis()) +
+  scale_x_continuous(position = "bottom", expand = c(0,0), labels = function(x)x/1000000, breaks = c(2100000,2400000,2700000), sec.axis = dup_axis()) +
   labs(x = "hap13", y = "hap25") +
   coord_cartesian(xlim = c(2000000,2850000), ylim = c(1850000,2800000)) +
-  theme(axis.text = element_text(color = "black", size = 6),
-        axis.title.x = element_text(color = "black", size = 8),
-        axis.title.y = element_text(color = "black", size = 8),
+  theme(axis.text = element_text(color = "black", size = 8),
+        axis.title.y.right = element_text(color = "black", size = 10, margin = margin(l = 0.01)),
+        axis.title.y.left = element_blank(),
+        axis.title.x.top = element_text(color = "black", size = 10, margin = margin(l = 0.01)),
+        axis.title.x.bottom = element_blank(),
+        axis.text.x.top = element_blank(),
+        axis.ticks.x.top = element_blank(),
+        axis.text.y.right = element_blank(),
+        axis.ticks.y.right = element_blank(),
+        panel.grid.major = element_line(color = "grey70", linewidth = 0.3, linetype = "dashed"),
         panel.background = element_blank(),
-        legend.position = "none",
-        panel.border = element_rect(fill = NA, color = "black", linetype = "dashed", linewidth = 0.1),
-        legend.text = element_text(size = 8),
-        legend.title = element_text(size = 8),
-        legend.key.height = unit(0.4, units = "cm"),
-        legend.key.width = unit(0.2, units = "cm"),
-        plot.margin = unit(c(0.3,0.3,0.3,0.3), units = "lines"),
-        legend.box.margin=margin(-5,-5,-5,-5))
+        panel.border = element_rect(fill = NA, color = "black", linewidth = 0.5),
+        plot.margin = margin(b = 4, r = 0.1, l = 0.1))
 
-hap6vshap23_plot <- ggplot(data = hap6vshap23) +
-  geom_segment(data = hap6vshap23, aes(x = qstart, xend = qend, y = tstart, yend = tend), linewidth = 0.1) +
-  scale_y_continuous(position = "left", expand = c(0,0), labels = function(x)x/1000000) +
-  scale_x_continuous(position = "bottom", expand = c(0,0), labels = function(x)x/1000000) +
-  scale_color_gradient(low = "#D5D5D5", high = "#000000", aesthetics = "color", name = "% identity", limits = c(0,100), breaks = c(0,25,50,75,100), labels = c("0","25","50","75","100")) +
+hap26vshap27chr9_plot <- ggplot(data = hap26vshap27chr9) +
+  geom_segment(data = hap26vshap27chr9, aes(x = qstart, xend = qend, y = tstart, yend = tend), color = "black", linewidth = 0.3) +
+  scale_y_continuous(position = "left", expand = c(0,0), labels = function(x)x/1000000, breaks = c(1700000,2000000,2300000), sec.axis = dup_axis()) +
+  scale_x_continuous(position = "bottom", expand = c(0,0), labels = function(x)x/1000000, breaks = c(1700000,1900000,2100000), sec.axis = dup_axis()) +
   coord_cartesian(xlim = c(1600000,2200000), ylim = c(1600000,2400000)) +
   labs(x = "hap27", y = "hap26") +
-  theme(axis.text = element_text(color = "black", size = 6),
-        axis.title = element_text(color = "black", size = 8),
-        strip.background = element_rect(fill = "white", linewidth = 0.1, colour = "black"),
-        legend.title = element_text(color = "black", size = 6),
-        legend.text = element_text(color = "black", size = 6),
+  theme(panel.background = element_rect(fill = NA),
+        panel.border = element_rect(fill = NA, color = "black", linewidth = 0.5),
+        axis.text = element_text(color = "black", size = 8),
+        axis.title.y.right = element_text(color = "black", size = 10, margin = margin(l = 0.01)),
+        axis.title.y.left = element_blank(),
+        axis.title.x.top = element_text(color = "black", size = 10, margin = margin(l = 0.01)),
+        axis.title.x.bottom = element_blank(),
+        axis.text.x.top = element_blank(),
+        axis.ticks.x.top = element_blank(),
+        axis.text.y.right = element_blank(),
+        axis.ticks.y.right = element_blank(),
+        panel.grid.major = element_line(color = "grey70", linewidth = 0.3, linetype = "dashed"),
+        plot.margin = margin(b = 2, t = 2, r = 1, l = 1))
+
+hap26vshap13chr9_plot <- ggplot(data = hap26vshap13chr9) +
+  geom_segment(data = hap26vshap13chr9, aes(x = qstart, xend = qend, y = tstart, yend = tend), color = "black", linewidth = 0.3) +
+  scale_y_continuous(position = "left", expand = c(0,0), labels = function(x)x/1000000, breaks = c(1700000,2100000,2500000), sec.axis = dup_axis()) +
+  scale_x_continuous(position = "bottom", expand = c(0,0), labels = function(x)x/1000000, breaks = c(1800000,2300000,2800000), sec.axis = dup_axis()) +
+  coord_cartesian(xlim = c(1600000,3000000), ylim = c(1500000,2700000)) +
+  labs(x = "hap13", y = "hap26") +
+  theme(axis.text = element_text(color = "black", size = 8),
+        axis.title.y.right = element_text(color = "black", size = 10, margin = margin(l = 0.01)),
+        axis.title.y.left = element_blank(),
+        axis.title.x.top = element_text(color = "black", size = 10, margin = margin(l = 0.01)),
+        axis.title.x.bottom = element_blank(),
+        axis.text.x.top = element_blank(),
+        axis.ticks.x.top = element_blank(),
+        axis.text.y.right = element_blank(),
+        axis.ticks.y.right = element_blank(),
+        panel.grid.major = element_line(color = "grey70", linewidth = 0.3, linetype = "dashed"),
         panel.background = element_blank(),
-        legend.position = "none",
-        panel.border = element_rect(fill = NA, color = "black", linetype = "dashed", linewidth = 0.1),
-        plot.margin = unit(c(0.5,0.3,0.3,0.3), units = "lines"),
-        legend.box.margin=margin(-5,-5,-5,-5))
+        panel.border = element_rect(fill = NA, color = "black", linewidth = 0.5),
+        plot.margin = margin(t = 4, r = 1, l = 1))
 
-combo_plot <- ggarrange(hap8vshap21_plot, hap6vshap23_plot, hap5vshap24_plot, ncol = 1, nrow = 3, heights = c(1,1,1))
+combo_plot <- ggarrange(hap25vshap13chr9_plot, hap26vshap27chr9_plot, hap26vshap13chr9_plot, ncol = 1, nrow = 3, heights = c(1,1,1))
 
-ggsave("Figure2b.tiff", combo_plot, device = "tiff", width = 1.5, height = 4, units = "in", dpi = 600)
+ggsave("Figure2b.tiff", combo_plot, device = "tiff", width = 1.8, height = 4.75, units = "in", dpi = 600)
--- Figure5/Figure5_SupFig10.R
+++ Figure5/Figure5_SupFig10.R
@@ -55,19 +55,20 @@ chrlen_melt$HAP <- factor(chrlen_melt$HAP, levels = c("hap3","hap4","hap25","hap
 
 ## Figure 5
 
+## Figure 5
+
 chrlen_aus <- subset(chrlen_melt, !(HAP %in% c("hap9","hap10","hap11","hap12","hap13","hap14","hap15","hap16","hap17","hap18","hap19","hap20","hap1","hap2","hap21","hap22","hap24")))
 
 aus_intervals <- subset(merged_intervals, !(HAP %in% c("hap9","hap10","hap11","hap12","hap13","hap14","hap15","hap16","hap17","hap18","hap19","hap20","hap1","hap2","hap21","hap22","hap24")))
-aus_intervals <- subset(aus_intervals, !(REFHAP %in% c("hap9","hap10","hap11","hap12","hap13","hap14","hap15","hap16","hap17","hap18","hap19","hap20","hap1","hap2","hap21","hap22","hap24")))
+aus_intervals <- subset(aus_intervals, !(REFHAP %in% c("hap9","hap10","hap11","hap12","hap13","hap14","hap15","hap16","hap17","hap18","hap19","hap20","hap1","hap2","hap21","hap22","hap24","hap6","hap32","hap25","hap31","hap7","hap8","hap29","hap30","hap27","hap28")))
 
 final_aus <- full_join(aus_intervals, chrlen_aus, by = c("HAP","CHROM"))
 final_aus$HAP <- factor(final_aus$HAP)
 just_aus <- final_aus
 
-just_aus$HAP <- factor(just_aus$HAP, levels = c("hap26","hap5","hap31","hap32","hap7","hap8","hap29","hap30","hap27","hap28","hap25","hap3","hap4","hap6","hap23"))
+just_aus$HAP <- factor(just_aus$HAP, levels = c("hap3","hap4","hap25","hap26","hap5","hap31","hap32","hap7","hap8","hap29","hap30","hap27","hap28","hap6","hap23"))
 just_aus$CHROM <- factor(just_aus$CHROM, levels = c("chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr10","chr11","chr12","chr13","chr14","chr15","chr16","chr17","chr18"))
 chrlen_aus$HAP <- factor(chrlen_aus$HAP, levels = c("hap3","hap4","hap25","hap26","hap5","hap7","hap8","hap27","hap28","hap29","hap30","hap31","hap32","hap6","hap23"))
-chrlen_aus$HAP <- factor(chrlen_aus$HAP, levels = c("hap26","hap5","hap31","hap32","hap7","hap8","hap29","hap30","hap27","hap28","hap25","hap3","hap4","hap6","hap23"))
 just_aus$REFHAP <- factor(just_aus$REFHAP)
 
 
@@ -76,43 +77,44 @@ plot2 <- ggplot(data = just_aus, aes(y = HAP)) +
   geom_point(aes(x = len+15000), y = 1.5, color = "white") +
   geom_point(x = -15000, y = -0.5, color = "white") +
   geom_point(x = -15000, y = 1.5, color = "white") +
-  ggchicklet:::geom_rrect(data = chrlen_aus, aes(xmax = len, fill = HAP), xmin = 1, ymin = 0, ymax = 1, r = unit(0.3, 'npc')) +
-  geom_segment(aes(x = start, xend = end, color = REFHAP, group = HAP), y = 0.5, yend = 0.5, linewidth = 3.7, key_glyph = "pointrange") +
-  ggchicklet:::geom_rrect(data= chrlen_aus, aes(xmax = len+15000), xmin = -14999, ymin = -0.1, ymax = 1.1, r = unit(0.3, 'npc'), color = "white", fill = NA, size = 0.3) +
-  facet_grid(HAP ~ CHROM, labeller = labeller(CHROM = chr_labels), scales = "free_x", space = "free_x", switch = "both", margins = FALSE) +
+  ggchicklet:::geom_rrect(data = chrlen_aus, aes(xmax = len, fill = HAP), xmin = 1, ymin = 0, ymax = 1, r = unit(0.2, 'npc')) +
+  geom_segment(data = na.omit(just_aus), aes(x = start, xend = end, color = REFHAP, group = HAP), y = 0.5, yend = 0.5, linewidth = 4.6, key_glyph = "pointrange") +
+  ggchicklet:::geom_rrect(data= chrlen_aus, aes(xmax = len+15000), xmin = -14999, ymin = -0.1, ymax = 1.1, r = unit(0.2, 'npc'), color = "white", fill = NA, size = 0.3) +
+  facet_grid(HAP ~ CHROM, labeller = labeller(CHROM = chr_labels), scales = "free_x", space = "free_x", switch = "y", margins = FALSE) +
   scale_x_continuous(expand = c(0.01,0.01)) +
   scale_y_discrete(limits=rev, expand = c(0.01,0.01)) +
   scale_color_manual(breaks = c("hap3","hap4","hap25","hap26","hap5","hap7","hap8",
                                 "hap27","hap28","hap29","hap30","hap31","hap32","hap6","hap23"),
-                     values = c("#FF0000","#FFFB00","black","#FF8000","#30D500","#00FFE8","#002EFF",
-                                "#AE00FF","black","black","black","black","black","black","black"), name = "Color key", drop = TRUE, limits = c("hap3","hap4","hap26","hap5","hap7","hap8"), na.value = "black") +
+                     values = c("#FA0348","#C581FC","grey20","#FA8203","#5EE3EF","grey20","grey20",
+                                "grey20","grey20","grey20","grey20","grey20","grey20","grey20","grey20"), name = "Color key", drop = TRUE, limits = c("hap3","hap4","hap26","hap5")) +
   scale_fill_manual(breaks = c("hap3","hap4","hap25","hap26","hap5","hap7","hap8",
                                "hap27","hap28","hap29","hap30","hap31","hap32","hap6","hap23"),
-                    values = c("#FF0000","#FFFB00","black","#FF8000","#30D500","#00FFE8","#002EFF",
-                               "#AE00FF","black","black","black","black","black","black","black"), name = "Color key", drop = TRUE, guide = "none") +
-  labs(x = "Chromosome", y = NULL) +
+                    values = c("#FA0348","#C581FC","grey20","#FA8203","#5EE3EF","grey20","grey20",
+                               "grey20","grey20","grey20","grey20","grey20","grey20","grey20","grey20"), name = "Color key", drop = TRUE, guide = "none") +
+  labs(x = NULL, y = NULL) +
   coord_cartesian(ylim = c(-0.6,1.6)) + 
   theme(panel.background = element_blank(),
         axis.text.y = element_blank(),
         axis.ticks.y = element_blank(),
-        strip.text.y.left = element_text(angle = 0, size = 7, hjust = 1),
-        strip.text.x.bottom = element_text(size = 7, vjust = 1),
+        strip.text.y.left = element_text(angle = 0, size = 10, hjust = 1),
+        strip.text.x.top = element_text(size = 10, vjust = 1, hjust = 0.5, margin = margin(t = 1, b = 0, l = 0, r = 0)),
         strip.background.y = element_blank(),
         axis.text.x = element_blank(),
-        legend.text = element_text(size = 7),
-        legend.title = element_text(size = 8),
-        axis.title.x = element_text(size = 8),
+        legend.text = element_text(size = 8),
+        legend.title = element_blank(),
         legend.key = element_blank(),
-        legend.key.size = unit(c(0.15), units = "in"),
-        plot.margin = unit(c(0,0,2.25,0), units = "lines"),
+        legend.key.size = unit(c(0.1), units = "in"),
+        plot.margin = unit(c(0,0,1.1,0), units = "lines"),
         panel.spacing = unit(0, units = "lines"),
-        legend.position = c(-0.07,-0.1),
+        legend.position = c(0.7,-0.01),
+        legend.background = element_blank(),
         legend.justification = "left",
         strip.background.x = element_blank(),
         axis.ticks.x = element_blank()) +
-  guides(color = guide_legend(nrow = 3, override.aes = list(size = 1, fill = NA, linetype = 0)))
+  guides(color = guide_legend(nrow = 1, override.aes = list(size = 1, fill = NA, linetype = 0)))
 
-ggsave("Figure5_pangenome_blocks.tiff", plot2, device = "tiff", width = 7, height = 6, units = "in", dpi = 600)
+ggsave("Figure5_pangenome_blocks.tiff", plot2, device = "tiff", width = 7.5, height = 6, units = "in", dpi = 600)
+
 
 
 ## Supplemental Figure 10
@@ -122,45 +124,46 @@ plot <- ggplot(data = keepme, aes(y = HAP)) +
   geom_point(aes(x = len+50000), y = 1.5, color = "white") +
   geom_point(x = -50000, y = -0.5, color = "white") +
   geom_point(x = -50000, y = 1.5, color = "white") +
-  ggchicklet:::geom_rrect(data = chrlen_melt, aes(xmax = len, color = HAP, fill = HAP), xmin = 1, ymin = 0, ymax = 1, r = unit(0.3, 'npc')) +
-  geom_segment(aes(x = start, xend = end, color = REFHAP, group = HAP), y = 0.5, yend = 0.5, linewidth = 3.3, key_glyph = "pointrange") +
+  ggchicklet:::geom_rrect(data = chrlen_melt, aes(xmax = len, fill = HAP), xmin = 1, ymin = 0, ymax = 1, r = unit(0.3, 'npc')) +
+  geom_segment(aes(x = start, xend = end, color = REFHAP, group = HAP), y = 0.5, yend = 0.5, linewidth = 3.7, key_glyph = "pointrange") +
   ggchicklet:::geom_rrect(data= chrlen_melt, aes(xmax = len+50000), xmin = -49999, ymin = -0.1, ymax = 1.1, r = unit(0.3, 'npc'), color = "white", fill = NA, size = 0.35) +
-  facet_grid(HAP ~ CHROM, labeller = labeller(CHROM = chr_labels), scales = "free_x", space = "free_x", switch = "both", margins = FALSE) +
+  facet_grid(HAP ~ CHROM, labeller = labeller(CHROM = chr_labels), scales = "free_x", space = "free_x", switch = "y", margins = FALSE) +
   scale_x_continuous(expand = c(0.01,0.01)) +
   scale_y_discrete(limits=rev, expand = c(0.01,0.01)) +
   scale_color_manual(breaks = c("hap3","hap4","hap25","hap26","hap5","hap7","hap8",
                                 "hap27","hap28","hap29","hap30","hap31","hap32","hap14","hap1","hap2","hap13",
                                 "hap11","hap15","hap16","hap17","hap18","hap19","hap20","hap6","hap10","hap12","hap23","hap9"),
-                     values = c("#FF0000","#FFFB00","black","#FF8000","#30D500","#00FFE8","#002EFF",
-                                "#AE00FF","black","black","black","black","black","#B8E9FF","#FF00F7","#006023","#FFCD97",
-                                "#DDB8FF","#CFFFB8","#45C48C","#AEB431","#00B5D2","black","black","black","black","black","black","black"), name = "Color key", drop = TRUE) +
+                     values = c("#FA0348","#C581FC","grey20","#FA8203","#5EE3EF","grey20","grey20",
+                                "grey20","grey20","grey20","grey20","grey20","grey20","#006023","#FF00F7","#0056FF","#FFCD97",
+                                "#DDB8FF","#CFFFB8","#45C48C","#AEB431","#6BA9B4","grey20","grey20","grey20","grey20","grey20","grey20","grey20"), 
+                     name = "Color key", drop = TRUE, limits = c("hap3","hap4","hap26","hap5","hap14","hap1","hap2","hap13","hap11","hap15","hap16","hap17","hap18")) +
   scale_fill_manual(breaks = c("hap3","hap4","hap25","hap26","hap5","hap7","hap8",
                                "hap27","hap28","hap29","hap30","hap31","hap32","hap14","hap1","hap2","hap13",
                                "hap11","hap15","hap16","hap17","hap18","hap19","hap20","hap6","hap10","hap12","hap23","hap9"),
-                    values = c("#FF0000","#FFFB00","black","#FF8000","#30D500","#00FFE8","#002EFF",
-                               "#AE00FF","black","black","black","black","black","#B8E9FF","#FF00F7","#006023","#FFCD97",
-                               "#DDB8FF","#CFFFB8","#45C48C","#AEB431","#00B5D2","black","black","black","black","black","black","black"), name = "Color key", drop = TRUE, guide = "none") +
+                    values = c("#FA0348","#C581FC","grey20","#FA8203","#5EE3EF","grey20","grey20",
+                               "grey20","grey20","grey20","grey20","grey20","grey20","#006023","#FF00F7","#0056FF","#FFCD97",
+                               "#DDB8FF","#CFFFB8","#45C48C","#AEB431","#6BA9B4","grey20","grey20","grey20","grey20","grey20","grey20","grey20"), 
+                    name = "Color key", drop = TRUE, guide = "none") +
   coord_cartesian(ylim = c(-0.6,1.6)) + 
-  labs(x = "Chromosome", y = NULL) +
+  labs(x = NULL, y = NULL) +
   theme(panel.background = element_blank(),
         axis.text.y = element_blank(),
         axis.ticks.y = element_blank(),
-        strip.text.y.left = element_text(angle = 0, size = 7, hjust = 1),
-        strip.text.x.bottom = element_text(size = 7, vjust = 1),
+        strip.text.y.left = element_text(angle = 0, size = 10, hjust = 1),
+        strip.text.x.top = element_text(size = 10, vjust = 1, hjust = 0.5, margin = margin(t = 1, b = 0, l = 0, r = 0)),
         strip.background.y = element_blank(),
         axis.text.x = element_blank(),
-        legend.text = element_text(size = 7),
-        legend.title = element_text(size = 8),
-        axis.title.x = element_text(size = 8),
+        legend.text = element_text(size = 8),
+        legend.title = element_blank(),
         legend.key = element_blank(),
-        legend.key.size = unit(c(0.15), units = "in"),
-        plot.margin = unit(c(0,0,5,0), units = "lines"),
+        legend.key.size = unit(c(0.1), units = "in"),
+        plot.margin = unit(c(0,0,1.1,0), units = "lines"),
         panel.spacing = unit(0, units = "lines"),
-        legend.position = c(-0.06,-0.11),
+        legend.position = c(-0.085,-0.01),
+        legend.background = element_blank(),
         legend.justification = "left",
         strip.background.x = element_blank(),
-        axis.ticks.x = element_blank(),
-        legend.background = element_blank()) +
-  guides(color = guide_legend(nrow = 5, override.aes = list(size = 1, fill = NA, linetype = 0)))
+        axis.ticks.x = element_blank()) +
+  guides(color = guide_legend(nrow = 1, override.aes = list(size = 1, fill = NA, linetype = 0)))
 
 ggsave("Supplemental_figure10_pangenome_blocks.tiff", plot, device = "tiff", width = 7, height = 9, units = "in", dpi = 600)

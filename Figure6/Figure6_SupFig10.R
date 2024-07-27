281b5ac1123b8e6d07204202405970df0d2230e0
henni164 <51927997+henni164@users.noreply.github.com>
2024-07-27 10:31:53 +1000

    updates to figS10 code

--- Figure6/Figure6_SupFig10.R
+++ Figure6/Figure6_SupFig10.R
@@ -5,7 +5,7 @@ library(reshape2)
 library(purrr)
 library(ggchicklet)
 
-## Fig6A, Fig6B, FigS10A, FigS10B
+## Fig6A, Fig6B, FigS10
 
 chr_labels <- c(
   chr1 = "1",
@@ -174,76 +174,8 @@ plot2 <- ggplot(data = just_aus, aes(y = HAP)) +
 ggsave("Figure6B.tiff", plot2, device = "tiff", width = 7.5, height = 6, units = "in", dpi = 600)
 
 
-
-## Figure S12 subset a
-merged_intervals <- read.delim("all_intervals_figS10A.txt", sep = "\t", header = FALSE)
-
-colnames(merged_intervals) <- c("CHROM","start","end","HAP","dens","REFHAP")
-merged_intervals <- merged_intervals[,c(4,1,2,3,6)]
-
-final_joined <- full_join(merged_intervals, chrlen_melt, by = c("HAP","CHROM"))
-final_joined$HAP <- factor(final_joined$HAP)
-figures10_revised <- final_joined
-
-
-figS10_chr_sub <- subset(chrlen_melt, HAP %in% c("hap3","hap4","hap5","hap26","hap9","hap14"))
-
-figureS10_revised$HAP <- factor(figureS10_revised$HAP, levels =c("hap3","hap4","hap5","hap26","hap9","hap14"))
-figureS10_revised$REFHAP <- factor(figureS10_revised$REFHAP, levels =c("hap3","hap4","hap5","hap26","hap9","hap14"))
-figureS10_revised$CHROM <- factor(figureS10_revised$CHROM, levels = c("chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8",
-                                                                      "chr9","chr10","chr11","chr12","chr13","chr14","chr15","chr16","chr17","chr18"))
-figS10_chr_sub$HAP <- factor(figS10_chr_sub$HAP, levels = c("hap3","hap4","hap5","hap26","hap9","hap14"))
-
-
-figureS10_revised <- figureS10_revised[!is.na(figureS10_revised$REFHAP),]
-
-
-## Supplemental Figure 12a
-
-plot3 <- ggplot(data = figureS10_revised, aes(y = HAP)) +
-  geom_point(aes(x = len+50000), y = -0.5, color = "white") +
-  geom_point(aes(x = len+50000), y = 1.5, color = "white") +
-  geom_point(x = -50000, y = -0.5, color = "white") +
-  geom_point(x = -50000, y = 1.5, color = "white") +
-  ggchicklet:::geom_rrect(data = figS10_chr_sub, aes(xmax = len, fill = HAP), xmin = 1, ymin = 0, ymax = 1, r = unit(0.1, 'npc')) +
-  geom_segment(aes(x = start, xend = end, color = REFHAP, group = HAP), y = 0.5, yend = 0.5, linewidth = 5.6, key_glyph = "pointrange") +
-  ggchicklet:::geom_rrect(data= figS10_chr_sub, aes(xmax = len+50000), xmin = -49999, ymin = -0.01, ymax = 1.01, r = unit(0.1, 'npc'), color = "black", fill = NA, size = 0.3) +
-  facet_grid(HAP ~ CHROM, labeller = labeller(CHROM = chr_labels), scales = "free_x", space = "free_x", switch = "y", margins = FALSE) +
-  scale_x_continuous(expand = c(0.01,700000)) +
-  scale_y_discrete(limits=rev, expand = c(0.01,0.01)) +
-  scale_color_manual(breaks = c("hap3","hap4","hap5","hap26","hap9","hap14"),
-                     values = c("#F45587","#FFA900","#0027D9","#6CAD00", "pink", "white"), 
-                     name = "Color key", drop = TRUE, limits = c("hap3","hap4","hap5","hap26","hap9")) +
-  scale_fill_manual(breaks = c("hap3","hap4","hap5","hap26","hap9","hap14"),
-                    values = c("#F45587","#FFA900","#0027D9","#6CAD00","pink","white"), 
-                    name = "Color key", drop = TRUE, guide = "none") +
-  coord_cartesian(ylim = c(-0.6,1.6)) + 
-  labs(x = NULL, y = NULL) +
-  theme(panel.background = element_blank(),
-        axis.text.y = element_blank(),
-        axis.ticks.y = element_blank(),
-        strip.text.y.left = element_text(angle = 0, size = 10, hjust = 1),
-        strip.text.x.top = element_text(size = 10, vjust = 1, hjust = 0.5, margin = margin(t = 1, b = 0, l = 0, r = 0)),
-        strip.background.y = element_blank(),
-        axis.text.x = element_blank(),
-        legend.text = element_text(size = 8),
-        legend.title = element_blank(),
-        legend.key = element_blank(),
-        legend.key.size = unit(c(0.1), units = "in"),
-        plot.margin = unit(c(0,0,1.1,0), units = "lines"),
-        panel.spacing = unit(0, units = "lines"),
-        legend.position = c(-0.01,-0.01),
-        legend.background = element_blank(),
-        legend.justification = "left",
-        strip.background.x = element_blank(),
-        axis.ticks.x = element_blank()) +
-  guides(color = guide_legend(nrow = 1, override.aes = list(size = 1, fill = NA, linetype = 0)))
-
-
-ggsave("FigureS10A.tiff", plot3, device = "tiff", width = 7.5, height = 2.7, units = "in", dpi = 600)
-
-## Figure S10B
-merged_intervals <- read.delim("all_intervals_figS10B.txt", sep = "\t", header = FALSE)
+## Figure S10
+merged_intervals <- read.delim("FigS10_intervals.txt", sep = "\t", header = FALSE)
 
 colnames(merged_intervals) <- c("CHROM","start","end","HAP","dens","REFHAP")
 merged_intervals <- merged_intervals[,c(4,1,2,3,6)]
@@ -253,18 +185,18 @@ final_joined$HAP <- factor(final_joined$HAP)
 figureS10_revised <- final_joined
 
 
-figS10_chr_sub <- subset(chrlen_melt, HAP %in% c("hap1","hap2","hap10","hap11","hap12","hap13","hap15","hap16","hap17","hap18","hap19","hap20","hap6","hap23"))
+figS10_chr_sub <- subset(chrlen_melt, HAP %in% c("hap1","hap2","hap9","hap10","hap11","hap12","hap13","hap14","hap15","hap16","hap17","hap18","hap19","hap20","hap5","hap6","hap23"))
 
-figureS10_revised$HAP <- factor(figureS10_revised$HAP, levels =c("hap1","hap2","hap10","hap11","hap12","hap13","hap15","hap16","hap17","hap18","hap19","hap20","hap6","hap23"))
-figureS10_revised$REFHAP <- factor(figureS10_revised$REFHAP, levels =c("hap1","hap2","hap10","hap11","hap12","hap13","hap15","hap16","hap17","hap18","hap19","hap20","hap6","hap23"))
+figureS10_revised$HAP <- factor(figureS10_revised$HAP, levels =c("hap1","hap2","hap9","hap10","hap11","hap12","hap13","hap14","hap15","hap16","hap17","hap18","hap19","hap20","hap5","hap6","hap23"))
+figureS10_revised$REFHAP <- factor(figureS10_revised$REFHAP, levels =c("hap1","hap2","hap9","hap10","hap11","hap12","hap13","hap14","hap15","hap16","hap17","hap18","hap19","hap20","hap5","hap6","hap23"))
 figureS10_revised$CHROM <- factor(figureS10_revised$CHROM, levels = c("chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr10","chr11","chr12","chr13","chr14","chr15","chr16","chr17","chr18"))
-figS10_chr_sub$HAP <- factor(figS10_chr_sub$HAP, levels = c("hap1","hap2","hap10","hap11","hap12","hap13","hap15","hap16","hap17","hap18","hap19","hap20","hap6","hap23"))
+figS10_chr_sub$HAP <- factor(figS10_chr_sub$HAP, levels = c("hap1","hap2","hap9","hap10","hap11","hap12","hap13","hap14","hap15","hap16","hap17","hap18","hap19","hap20","hap5","hap6","hap23"))
 
 
 figureS10_revised <- figureS10_revised[!is.na(figureS10_revised$REFHAP),]
 
 
-## Supplemental Figure 12b
+## Supplemental Figure 10
 
 plot3 <- ggplot(data = figureS10_revised, aes(y = HAP)) +
   geom_point(aes(x = len+50000), y = -0.5, color = "white") +
@@ -277,16 +209,16 @@ plot3 <- ggplot(data = figureS10_revised, aes(y = HAP)) +
   facet_grid(HAP ~ CHROM, labeller = labeller(CHROM = chr_labels), scales = "free_x", space = "free_x", switch = "y", margins = FALSE) +
   scale_x_continuous(expand = c(0.03,0.01)) +
   scale_y_discrete(limits=rev, expand = c(0.01,0.01)) +
-  scale_color_manual(breaks = c("hap1","hap2","hap10","hap11","hap12","hap13",
-                                "hap15","hap16","hap17","hap18","hap19","hap20","hap6","hap23"),
-                     values = c("#FF00F7","#00E095","black","#FFCD97", "#DDB8FF", "grey30", "#006023",
-                                "#CFFFB8","#71E2ED","#AEB431","#6BA9B4","grey70","white","white","white"), 
-                     name = "Color key", drop = TRUE, limits = c("hap1","hap2","hap10","hap11","hap12","hap13",
-                                                                 "hap15","hap16","hap17","hap18","hap19")) +
-  scale_fill_manual(breaks = c("hap1","hap2","hap10","hap11","hap12","hap13",
-                               "hap15","hap16","hap17","hap18","hap19","hap20","hap6","hap23"),
-                    values = c("#FF00F7","#00E095","black","#FFCD97", "#DDB8FF", "grey30", "#006023",
-                               "#CFFFB8","#71E2ED","#AEB431","#6BA9B4","grey70","white","white","white"), 
+  scale_color_manual(breaks = c("hap1","hap2","hap9","hap10","hap11","hap12","hap13","hap14",
+                                "hap15","hap16","hap17","hap18","hap19","hap20","hap5","hap6","hap23"),
+                     values = c("#FF00F7","#00E095","black", "#ffc1f8", "#FFCD97", "#b7a2ff", "grey30", "#da8a16", "#006023",
+                                "#CFFFB8","#71E2ED","#AEB431","#6BA9B4","grey70","#fff63b","white","white","white"), 
+                     name = "Color key", drop = TRUE, limits = c("hap1","hap2","hap9","hap10","hap11","hap12","hap13","hap14",
+                                                                 "hap15","hap16","hap17","hap18","hap19","hap20")) +
+  scale_fill_manual(breaks = c("hap1","hap2","hap9","hap10","hap11","hap12","hap13","hap14",
+                               "hap15","hap16","hap17","hap18","hap19","hap20","hap5","hap6","hap23"),
+                    values = c("#FF00F7","#00E095","black", "#ffc1f8", "#FFCD97", "#b7a2ff", "grey30","#da8a16", "#006023",
+                               "#CFFFB8","#71E2ED","#AEB431","#6BA9B4","grey70","#fff63b","white","white","white"), 
                     name = "Color key", drop = TRUE, guide = "none") +
   coord_cartesian(ylim = c(-0.6,1.6)) + 
   labs(x = NULL, y = NULL) +
@@ -301,14 +233,14 @@ plot3 <- ggplot(data = figureS10_revised, aes(y = HAP)) +
         legend.title = element_blank(),
         legend.key = element_blank(),
         legend.key.size = unit(c(0.1), units = "in"),
-        plot.margin = unit(c(0,0,1.1,0), units = "lines"),
+        plot.margin = unit(c(0,0,2,0), units = "lines"),
         panel.spacing = unit(0, units = "lines"),
-        legend.position = c(-0.01,-0.01),
+        legend.position = c(-0.01,-0.03),
         legend.background = element_blank(),
         legend.justification = "left",
         strip.background.x = element_blank(),
         axis.ticks.x = element_blank()) +
-  guides(color = guide_legend(nrow = 1, override.aes = list(size = 1, fill = NA, linetype = 0)))
+  guides(color = guide_legend(nrow = 2, override.aes = list(size = 1, fill = NA, linetype = 0)))
 
 
-ggsave("FigureS10B.tiff", plot3, device = "tiff", width = 7.5, height = 5.5, units = "in", dpi = 600)
+ggsave("FigureS10.tiff", plot3, device = "tiff", width = 7.5, height = 7, units = "in", dpi = 600)
